42b402b6d9d1e5b2b432c9fd648f07252d91b508
Zijing Zhang <zzj0402@gmail.com>
2018-05-22 19:46:42 +0800

    interrupted

---
+++ testSum.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+//TestSum tests sum function
+func TestSum(t *testing.T) {
+	total := Sum(5, 5)
+	if total != 10 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	}
+}
